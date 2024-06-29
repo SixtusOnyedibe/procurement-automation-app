9536f0339118d229dd8ce8e4dbdcd7a403e89783
Sixtus Onyedibe <sixtusonyedibe@gmail.com>
2024-06-29 07:09:17 +0100

    Updated db service to matche current user data structure

--- backend/services/db.service.js
+++ backend/services/db.service.js
@@ -3,7 +3,6 @@ import fs from 'fs';
 const dbPath = './storage/db.json';
 
 export const readOrders = () => {
-  // const path = "../storage/db.s"
   const data = fs.readFileSync(dbPath, 'utf8');
   return JSON.parse(data);
 };
@@ -11,23 +10,4 @@ export const readOrders = () => {
 export const writeOrders = (data) => {
   const jsonData = JSON.stringify(data, null, 2);
   fs.writeFileSync(dbPath, jsonData, 'utf8');
-};
-
-export const addOrder = (customerId, newOrder) => {
-  const data = readOrders();
-
-  const user = data.users.find((user) => user.customerId === customerId);
-
-  if (user) {
-    user.orders.push(newOrder);
-  } else {
-    data.users.push({
-      customerId,
-      name: 'New User',
-      email: 'newuser@example.com',
-      orders: [newOrder],
-    });
-  }
-
-  writeOrders(data);
 };
