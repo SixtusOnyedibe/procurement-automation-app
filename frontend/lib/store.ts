df980ae7ee2a795cbffbfb018e3b5cbea831f3a8
Sixtus Onyedibe <sixtusonyedibe@gmail.com>
2024-06-30 16:39:18 +0100

    Moved the menu state to not persist

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -10,8 +10,14 @@ interface User {
 interface userStore {
   user: User | null;
   setUser: (value: User | null) => void;
+}
+
+interface MenuStore {
   accountComponentOpen: boolean;
   setAccountComponentOpen: (value: boolean) => void;
+
+  burgerOpen: boolean;
+  setBurgerOpen: (value: boolean) => void;
 }
 
 const userStore = create<userStore>()(
@@ -19,13 +25,18 @@ const userStore = create<userStore>()(
     (set) => ({
       user: null,
       setUser: (value) => set(() => ({ user: value })),
-
-      accountComponentOpen: false,
-      setAccountComponentOpen: (value) =>
-        set(() => ({ accountComponentOpen: value })),
     }),
     { name: 'procurement-user' }
   )
 );
+
+export const menuStore = create<MenuStore>()((set) => ({
+  accountComponentOpen: false,
+  setAccountComponentOpen: (value) =>
+    set(() => ({ accountComponentOpen: value })),
+
+  burgerOpen: false,
+  setBurgerOpen: (value) => set(() => ({ burgerOpen: value })),
+}));
 
 export default userStore;
